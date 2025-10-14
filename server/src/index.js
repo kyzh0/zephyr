551604168a2c805cc69858cc20b5e0c0bc32a701
Kyle Zhou <kzkyle2000@gmail.com>
2025-10-14 19:23:28 +0800

    separate app.js index.js

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+import app from './app.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import logger from './lib/log.js';
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.NODE_ENV === 'production'
+    ? process.env.DB_CONNECTION_STRING
+    : process.env.DEV_CONNECTION_STRING
+);
+
+const port = process.env.NODE_PORT || 5000;
+app.listen(port, () => logger.info(`Server running on port ${port}`));
