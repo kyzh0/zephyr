cbf7f9f713e1091b676b8fa0a901f53d05f6cf4d
Kyle Zhou <kzkyle2000@gmail.com>
2025-10-14 21:38:21 +0800

    new attentis scraper

---
+++ server/src/scrapers/stations/types/attentis.js
@@ -0,0 +1,53 @@
+import { httpClient } from '../../../lib/httpClient.js';
+import { processScrapedData } from '../processScrapedData.js';
+import logger from '../../../lib/log.js';
+
+export default async function getAttentisData(stations) {
+  try {
+    const result = [];
+    const { data } = await httpClient.get('https://api.attentistechnology.com/sensor-overview', {
+      headers: { Authorization: `Bearer ${process.env.ATTENTIS_KEY}` }
+    });
+    if (data.data && data.data.weather_readings) {
+      for (var key of Object.keys(data.data.weather_readings)) {
+        const d = data.data.weather_readings[key];
+        result.push({
+          id: key,
+          data: {
+            windAverage: d.wind_speed,
+            windGust: d.wind_gust_speed,
+            windBearing: d.wind_direction,
+            temperature: d.air_temp
+          }
+        });
+      }
+    }
+
+    for (const station of stations) {
+      const d = result.find((x) => x.id === station.externalId);
+      if (d) {
+        await processScrapedData(
+          station,
+          d.data.windAverage,
+          d.data.windGust,
+          d.data.windBearing,
+          d.data.temperature
+        );
+      } else {
+        logger.warn(
+          `An error occured while fetching data for attentis - no data for ${station.externalId}`,
+          {
+            service: 'station',
+            type: 'attentis'
+          }
+        );
+      }
+    }
+  } catch (error) {
+    logger.warn('An error occured while fetching data for attentis', {
+      service: 'station',
+      type: 'attentis'
+    });
+    logger.warn(error);
+  }
+}
