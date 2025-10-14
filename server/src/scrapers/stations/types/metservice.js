2e06733b2284f37651ad1fe1f0aba7e892aeceaa
Kyle Zhou <kzkyle2000@gmail.com>
2025-10-14 21:30:10 +0800

    metservice - plimit concurrency

--- server/src/scrapers/stations/types/metservice.js
+++ server/src/scrapers/stations/types/metservice.js
@@ -1,45 +1,57 @@
+import pLimit from 'p-limit';
 import { httpClient } from '../../../lib/httpClient.js';
-import logger from '../../../lib/log.js';
 import { getWindBearingFromDirection } from '../../../lib/utils.js';
 import { processScrapedData } from '../processScrapedData.js';
+import logger from '../../../lib/log.js';
 
-export default async function getMetserviceData(station) {
-  try {
-    let windAverage = null;
-    let windGust = null;
-    let windBearing = null;
-    let temperature = null;
+export default async function getMetserviceData(stations) {
+  const limit = pLimit(10);
 
-    const { data } = await httpClient.get(
-      `https://www.metservice.com/publicData/webdata/module/weatherStationCurrentConditions/${station.externalId}`
-    );
-    const wind = data.observations.wind;
-    if (wind && wind.length && wind[0]) {
-      windAverage = wind[0].averageSpeed;
-      windGust = wind[0].gustSpeed;
+  await Promise.allSettled(
+    stations.map((station) =>
+      limit(async () => {
+        try {
+          let windAverage = null;
+          let windGust = null;
+          let windBearing = null;
+          let temperature = null;
 
-      if (wind[0].strength === 'Calm') {
-        if (windAverage == null) {
-          windAverage = 0;
+          const { data } = await httpClient.get(
+            `https://www.metservice.com/publicData/webdata/module/weatherStationCurrentConditions/${station.externalId}`
+          );
+          const wind = data.observations.wind;
+          if (wind && wind.length && wind[0]) {
+            windAverage = wind[0].averageSpeed;
+            windGust = wind[0].gustSpeed;
+
+            if (wind[0].strength === 'Calm') {
+              if (windAverage == null) {
+                windAverage = 0;
+              }
+              if (windGust == null) {
+                windGust = 0;
+              }
+            }
+
+            windBearing = getWindBearingFromDirection(wind[0].direction);
+          }
+
+          const temp = data.observations.temperature;
+          if (temp && temp.length && temp[0]) {
+            temperature = temp[0].current;
+          }
+
+          await processScrapedData(station, windAverage, windGust, windBearing, temperature);
+        } catch (error) {
+          logger.warn(
+            `An error occured while fetching data for metservice - ${station.externalId}`,
+            {
+              service: 'station',
+              type: 'metservice'
+            }
+          );
         }
-        if (windGust == null) {
-          windGust = 0;
-        }
-      }
-
-      windBearing = getWindBearingFromDirection(wind[0].direction);
-    }
-
-    const temp = data.observations.temperature;
-    if (temp && temp.length && temp[0]) {
-      temperature = temp[0].current;
-    }
-
-    await processScrapedData(station, windAverage, windGust, windBearing, temperature);
-  } catch (error) {
-    logger.warn(`An error occured while fetching data for metservice - ${station.externalId}`, {
-      service: 'station',
-      type: 'metservice'
-    });
-  }
+      })
+    )
+  );
 }
