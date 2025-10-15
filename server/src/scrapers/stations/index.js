e641bb62346ecb6c6eee9256abc2c16cbfb6cfe9
Kyle Zhou <kzkyle2000@gmail.com>
2025-10-15 23:20:56 +0800

    tempest, wu

--- server/src/scrapers/stations/index.js
+++ server/src/scrapers/stations/index.js
@@ -1,9 +1,13 @@
 import scrapeAttentisData from './types/attentis.js';
 import scrapeHolfuyData from './types/holfuy.js';
 import scrapeMetserviceData from './types/metservice.js';
+import scrapeTempestData from './types/tempest.js';
+import scrapeWeatherUndergroundData from './types/weatherUnderground.js';
 
 export default {
   attentis: scrapeAttentisData,
   holfuy: scrapeHolfuyData,
-  metservice: scrapeMetserviceData
+  metservice: scrapeMetserviceData,
+  tempest: scrapeTempestData,
+  wu: scrapeWeatherUndergroundData
 };
