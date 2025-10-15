f713e201bef0cf8b92a0aac6a89da25dcdb731dd
Kyle Zhou <kzkyle2000@gmail.com>
2025-10-15 23:12:12 +0800

    holfuy scraper

--- server/src/scrapers/stations/index.js
+++ server/src/scrapers/stations/index.js
@@ -1,7 +1,9 @@
 import scrapeAttentisData from './types/attentis.js';
+import scrapeHolfuyData from './types/holfuy.js';
 import scrapeMetserviceData from './types/metservice.js';
 
 export default {
   attentis: scrapeAttentisData,
+  holfuy: scrapeHolfuyData,
   metservice: scrapeMetserviceData
 };
