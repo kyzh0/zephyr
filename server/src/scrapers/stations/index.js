8aceec8e0d0857c4a76bd437184f599e6743818f
Kyle Zhou <kzkyle2000@gmail.com>
2025-10-16 16:14:01 +0800

    po, cp

--- server/src/scrapers/stations/index.js
+++ server/src/scrapers/stations/index.js
@@ -1,7 +1,9 @@
 import scrapeAttentisData from './types/attentis.js';
-import scrapeCWUData from './types/cwu.js';
+import scrapeCwuData from './types/cwu.js';
+import scrapeCentrePortData from './types/centrePort.js';
 import scrapeHolfuyData from './types/holfuy.js';
 import scrapeMetserviceData from './types/metservice.js';
+import scrapePortOtagoData from './types/portOtago.js';
 import scrapeTempestData from './types/tempest.js';
 import scrapeWeatherProData from './types/weatherPro.js';
 import scrapeWeatherUndergroundData from './types/weatherUnderground.js';
@@ -10,9 +12,11 @@ import scrapeWowData from './types/wow.js';
 
 export default {
   attentis: scrapeAttentisData,
-  cwu: scrapeCWUData,
+  cwu: scrapeCwuData,
+  cp: scrapeCentrePortData,
   holfuy: scrapeHolfuyData,
   metservice: scrapeMetserviceData,
+  po: scrapePortOtagoData,
   tempest: scrapeTempestData,
   wp: scrapeWeatherProData,
   wu: scrapeWeatherUndergroundData,
