a734d65827d294d6b192ee162fb6d6d4473ec9bb
Kyle Zhou <kzkyle2000@gmail.com>
2025-10-16 19:58:22 +0800

    cams - metservice, lw

--- server/src/scrapers/cams/index.js
+++ server/src/scrapers/cams/index.js
@@ -1,7 +1,11 @@
 import scrapeHarvestData from './types/harvest.js';
+import scrapeLakeWanakaData from './types/lakeWanaka.js';
+import scrapeMetserviceData from './types/metservice.js';
 import scrapeSnowgrassData from './types/snowgrass.js';
 
 export default {
   harvest: scrapeHarvestData,
+  lw: scrapeLakeWanakaData,
+  metservice: scrapeMetserviceData,
   snowgrass: scrapeSnowgrassData
 };
