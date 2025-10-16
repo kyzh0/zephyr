f27b25a9ac7e47641f2a21f8489cdec0473c09e5
Kyle Zhou <kzkyle2000@gmail.com>
2025-10-16 20:11:37 +0800

    cams - cgc, wa

---
+++ server/src/scrapers/cams/types/cgc.js
@@ -0,0 +1,38 @@
+import pLimit from 'p-limit';
+import httpClient from '../../../lib/httpClient.js';
+import processScrapedData from '../processScrapedData.js';
+import logger from '../../../lib/logger.js';
+
+export default async function scrapeCgcData(cams) {
+  const limit = pLimit(10);
+
+  await Promise.allSettled(
+    cams.map((cam) =>
+      limit(async () => {
+        try {
+          let updated = null;
+          let base64 = null;
+
+          const response = await httpClient.get(
+            `https://canterburyglidingclub.nz/images/CGCHdCam${cam.externalId}_1.jpg`,
+            {
+              responseType: 'arraybuffer'
+            }
+          );
+          base64 = Buffer.from(response.data, 'binary').toString('base64');
+          updated = new Date();
+
+          await processScrapedData(cam, updated, base64);
+        } catch (error) {
+          logger.warn(
+            `An error occured while fetching data for wanaka airport - ${cam.externalId}`,
+            {
+              service: 'cam',
+              type: 'wa'
+            }
+          );
+        }
+      })
+    )
+  );
+}
