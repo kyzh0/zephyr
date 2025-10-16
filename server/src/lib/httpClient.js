e808c4e1faf7980b5f19c4e458a428a732bf7e77
Kyle Zhou <kzkyle2000@gmail.com>
2025-10-16 16:30:33 +0800

    sfo, navigatus

--- server/src/lib/httpClient.js
+++ server/src/lib/httpClient.js
@@ -3,7 +3,7 @@ import http from 'http';
 import https from 'https';
 
 const httpClient = axios.create({
-  timeout: 10000,
+  timeout: 15000,
   httpAgent: new http.Agent({ keepAlive: true }),
   httpsAgent: new https.Agent({ keepAlive: true })
 });
