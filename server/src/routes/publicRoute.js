d81d8d0a28d42c9535b2f1604a85d2048d4f1481
Kyle Zhou <kzkyle2000@gmail.com>
2025-10-18 19:59:55 +0800

    xlsx - worker thread

--- server/src/routes/publicRoute.js
+++ server/src/routes/publicRoute.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { formatInTimeZone } from 'date-fns-tz';
-import fs from 'fs/promises';
-import * as geofire from 'geofire-common';
+import { Worker } from 'worker_threads';
 
 import logger from '../lib/logger.js';
 import { Client } from '../models/clientModel.js';
@@ -11,6 +10,11 @@ import { Output } from '../models/outputModel.js';
 import * as XLSX from 'xlsx';
 import fs1 from 'fs';
 XLSX.set_fs(fs1);
+
+import { fileURLToPath, pathToFileURL } from 'url';
+import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const router = express.Router();
 
@@ -191,9 +195,8 @@ router.get('/json-output', async (req, res) => {
   res.json(result);
 });
 
-router.get('/csv', async (req, res) => {
-  logger.info('CSV requested', { service: 'public' });
-  const ts = Date.now();
+router.get('/xlsx', async (req, res) => {
+  logger.info('XLSX export requested', { service: 'public' });
 
   const unixFrom = Number(req.query.unixFrom);
   const unixTo = Number(req.query.unixTo);
@@ -214,82 +217,34 @@ router.get('/csv', async (req, res) => {
       return;
     }
 
-    let dateFrom = null;
-    let dateTo = new Date();
-    if (unixFrom && !isNaN(unixFrom)) {
-      dateFrom = new Date(unixFrom * 1000);
-    }
-    if (unixFrom && !isNaN(unixTo)) {
-      dateTo = new Date(unixTo * 1000);
-    }
-    // limit 6 months data
-    const ms180Days = 180 * 24 * 60 * 60 * 1000;
-    if (dateFrom == null || dateTo.getTime() - dateFrom.getTime() > ms180Days) {
-      dateFrom = new Date(dateTo.getTime() - ms180Days);
-    }
-
-    const output = await Output.find({
-      time: { $gte: dateFrom.getTime(), $lte: dateTo.getTime() }
-    }).sort({ time: 1 });
-
-    // read from json files
-    const data = {};
-    const stationNames = {};
-    for (const o of output) {
-      const i = o.url.indexOf('/data');
-      if (i < 0) continue;
-      const fileData = await fs.readFile(`public/${o.url.slice(i)}`, 'utf8');
-      const json = JSON.parse(fileData);
-      for (const line of json) {
-        // filter radius
-        const distance =
-          Math.round(
-            geofire.distanceBetween([line.coordinates.lat, line.coordinates.lon], [lat, lon]) * 10
-          ) / 10;
-        if (distance > radius) continue;
-
-        const d = {
-          timeUtc: new Date(line.timestamp * 1000).toISOString(),
-          windAvgKmh: line.wind.average,
-          windGustKmh: line.wind.gust,
-          windBearingDeg: line.wind.bearing,
-          temperatureC: line.temperature
-        };
-        if (data[line.id] == null) data[line.id] = [d];
-        else data[line.id].push(d);
-
-        if (stationNames[line.id] == null) stationNames[line.id] = line.name;
-      }
-    }
-
-    // write xlsx
-    const wb = XLSX.utils.book_new();
-    for (const key of Object.keys(data)) {
-      const readings = data[key];
-      readings.sort((a, b) => {
-        return new Date(a.timeUTC).getTime() - new Date(b.timeUTC).getTime();
-      });
-
-      const ws = XLSX.utils.json_to_sheet(readings);
-      XLSX.utils.book_append_sheet(wb, ws, stationNames[key]);
-    }
-
-    const dir = 'public/export';
-    await fs.mkdir(dir, { recursive: true });
-    const fileName = `zephyr-data-${Math.floor(Date.now() / 1000)}.xlsx`;
-    const filePath = `${dir}/${fileName}`;
-    XLSX.writeFile(wb, filePath);
-    logger.info(`CSV generated, ${Date.now() - ts}ms elapsed - ${fileName}`);
-
-    const urlPrefix =
-      process.env.NODE_ENV === 'production' ? 'https://fs.zephyrapp.nz/' : 'http://localhost:5000/';
-    res.json({
-      url: `${urlPrefix}${filePath}`
-    });
+    const url = await runExportDataWorker(unixFrom, unixTo, lat, lon, radius);
+    res.json({ url });
   } catch (error) {
     logger.error(error, { service: 'public' });
     res.status(500).json({ error: 'Something went wrong...' });
   }
 });
+
+function runExportDataWorker(unixFrom, unixTo, lat, lon, radius) {
+  return new Promise((resolve, reject) => {
+    const workerPath = path.resolve(__dirname, '../workers/exportDataWorker.js');
+    const worker = new Worker(pathToFileURL(workerPath), {
+      workerData: {
+        unixFrom,
+        unixTo,
+        lat,
+        lon,
+        radius
+      }
+    });
+    worker.on('message', resolve);
+    worker.on('error', reject);
+    worker.on('exit', (code) => {
+      if (code !== 0) {
+        reject(new Error(`Worker stopped with exit code ${code}`));
+      }
+    });
+  });
+}
 
 export default router;
