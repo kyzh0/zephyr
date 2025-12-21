7ee4a60a5509f8b71bfce982a2174387449b55fc
James Bayly <james@bayly.xyz>
2025-12-21 14:52:05 +1300

    Nearby-webcam (#28)
    * Add nearby webcams
    
    * Add nearby webcams to station data
    
    * Update background of closed state
    
    * Leave scroll at top
    
    * Update flex alignment

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,3 +1,10 @@
 export { useStationData, type TimeRange } from "./useStationData";
 export { useMousePosition } from "./useMousePosition";
 export { useIsMobile } from "./useIsMobile";
+export {
+  useWebcam,
+  useWebcams,
+  useNearbyWebcams,
+  type UseWebcamResult,
+  type UseNearbyWebcamsResult,
+} from "./useWebcam";
