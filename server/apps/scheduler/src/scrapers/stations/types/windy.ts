fdcd50de6b2ae9031aa3a0edb8c2d6bd69a72093
Kyle Zhou <kzkyle2000@gmail.com>
2026-03-05 10:04:29 +1300

    Windy new API

--- server/apps/scheduler/src/scrapers/stations/types/windy.ts
+++ server/apps/scheduler/src/scrapers/stations/types/windy.ts
@@ -4,10 +4,15 @@ import { httpClient, logger, type StationAttrs, type WithId } from '@zephyr/shar
 import processScrapedData from '../processScrapedData';
 
 type WindyResponse = {
-  time: string; // ISO timestamp
-  wind: number; // m/s
-  gust: number; // m/s
-  dir: number;
+  data: WindyData;
+};
+
+type WindyData = {
+  ts: number[];
+  temp: number[];
+  wind: number[];
+  windDir: number[];
+  gust: number[];
 };
 
 export default async function scrapeWindyData(stations: WithId<StationAttrs>[]): Promise<void> {
@@ -20,18 +25,29 @@ export default async function scrapeWindyData(stations: WithId<StationAttrs>[]):
           let windAverage: number | null = null;
           let windGust: number | null = null;
           let windBearing: number | null = null;
-          const temperature: number | null = null;
+          let temperature: number | null = null;
 
           const { data } = await httpClient.get<WindyResponse>(
-            `https://node.windy.com/pois/v2/wind/${station.externalId}`
+            `https://node.windy.com/obs/measurement/v3/pws/${station.externalId}/10/1`
           );
 
-          // skip if data is older than 20 mins
-          const ts = new Date(data.time).getTime();
-          if (!Number.isNaN(ts) && Date.now() - ts < 20 * 60 * 1000) {
-            windAverage = Math.round(data.wind * 3.6 * 100) / 100; // m/s -> km/h
-            windGust = Math.round(data.gust * 3.6 * 100) / 100;
-            windBearing = data.dir;
+          if (
+            data &&
+            data.data.ts.length &&
+            data.data.ts.length == data.data.wind.length &&
+            data.data.ts.length == data.data.windDir.length &&
+            data.data.ts.length == data.data.gust.length &&
+            data.data.ts.length == data.data.temp.length
+          ) {
+            const i = data.data.ts.length - 1;
+            const ts = new Date(data.data.ts[i]).getTime();
+            // skip if data is older than 20 mins
+            if (!Number.isNaN(ts) && Date.now() - ts < 20 * 60 * 1000) {
+              windAverage = Math.round(data.data.wind[i] * 3.6 * 100) / 100; // m/s -> km/h
+              windGust = Math.round(data.data.gust[i] * 3.6 * 100) / 100;
+              windBearing = data.data.windDir[i];
+              temperature = Math.round(data.data.temp[i] - 273.15); // K -> °C
+            }
           }
 
           await processScrapedData(station, windAverage, windGust, windBearing, temperature);
